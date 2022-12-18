a2a9cf5b321196336b0ea6edd65eac29209c0b3c
Sarah SmithAbdullah <tamanabdullah9@gmail.com>
2022-12-18 16:34:21 +0200

    After linking the Bridge Pattern with the Abstract Factory Pattern.

--- src/main/java/com/example/optimization/HelloApplication.java
+++ src/main/java/com/example/optimization/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
        Abstract anAbstract = new Abstract();
-       anAbstract.Welcome();
+       anAbstract.Welcome("phase one");
     }
 
     public static void main(String[] args) {
